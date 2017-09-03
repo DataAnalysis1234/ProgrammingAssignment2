969baf89d247bccda63994eab2ec65eb26b804d4
Connor Flood <floodconnor@gmail.com>
2017-09-03 12:05:08 -0400

    branch commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creates a cached matrix, that can be used in cachesolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat <- NULL
+  set <- function(y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inverse) invmat <<- inverse
+  getInv <- function() invmat
+  list(set = set, get = get, 
+       setInv = setInv, 
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Takes matrix created from makeCacheMatrix
+## and calculates the inverse of said matrix.
+## Additionally, it stores the value to save time
+## if you re-run it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invmat <- x$getInv()
+  if(!is.null(invmat)) {
+    message("pulling calculated data")
+    return(invmat)
+  }
+  data <- x$get()
+  invmat  <- solve(data,...)
+  x$setInv(invmat)
+  invmat
 }
